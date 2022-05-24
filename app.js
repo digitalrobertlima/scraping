19c1f88d34dfcfe31bad03cda2729eedfa6065e1
scr1pt3r <ge.crio123@gmail.com>
2022-05-24 01:21:05 -0300

    req blaze.com

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const https = require('https');
 
-const options = {
-	hostname: 'www.google.com.br',
+/*
+ * const options = {
+	hostname: 'www.blaze.com',
 	port: 443,
 	path: '/',
 	method: 'GET',
 };
+*/
 
-const request = https.request(options, response => {
+const request = https.request('https://blaze.com/sitemap.xml', 'GET', response => {
 	console.log(`statusCode: ${response.statusCode}`);
 
 	response.on('data', d => {
